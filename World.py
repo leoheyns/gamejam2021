476ce1558e6aa0dc790b6ddb92e8e4240da0181f
MarcoBorsato <woutvelthuis01@gmail.com>
2021-05-08 21:37:06 +0200

    Start menu en pause menu

--- World.py
+++ World.py
@@ -1,40 +1,38 @@
 from Room import Room
-from global_constants import * 
+from global_constants import *
+
 
 def attempt_move(coords, direction):
     if direction == 1:
         if coords[1] > 0:
-            return coords[0],coords[1]-1
+            return coords[0], coords[1] - 1
     elif direction == 2:
         if coords[0] < WORLD_DIM[0] - 1:
-            return coords[0] + 1,coords[1]
+            return coords[0] + 1, coords[1]
     elif direction == 3:
         if coords[1] < WORLD_DIM[1] - 1:
-            return coords[0],coords[1] + 1
+            return coords[0], coords[1] + 1
     elif direction == 4:
         if coords[0] > 0:
-            return coords[0] - 1,coords[1]
+            return coords[0] - 1, coords[1]
     return coords
 
 
 class World:
-
     rooms = []
-    current_coords = (0,0)
+    current_coords = (0, 0)
 
     def __init__(self):
         for x in range(WORLD_DIM[0]):
             self.rooms.append([])
             for y in range(WORLD_DIM[1]):
-                self.rooms[x].append(Room([True,True,True,True]))
-
+                self.rooms[x].append(Room([True, True, True, True]))
 
     def draw(self, WIN):
-        print(self.current_coords)
         self.rooms[self.current_coords[0]][self.current_coords[1]].draw(WIN)
-    
+
     def update(self):
         pass
 
-    def move(self,direction):
+    def move(self, direction):
         self.current_coords = attempt_move(self.current_coords, direction)
--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import pygame
 from World import World
 from global_constants import *
 
-
-
 FPS = 60
 SCALE = 3
 WIDTH, HEIGHT = ROOM_DIM[0] * TILESIZE, ROOM_DIM[1] * TILESIZE
 
 WIN = pygame.display.set_mode((WIDTH * SCALE, HEIGHT * SCALE))
-pygame.display.set_caption("wie dit leest trekt een ad")
+pygame.display.set_caption("Miel Monteur saves the world")
 
 world = World()
 
@@ -18,7 +16,7 @@ def draw():
     temp_win = pygame.Surface((WIDTH, HEIGHT))
     world.draw(temp_win)
     temp_win = pygame.transform.scale(temp_win, (WIDTH * 3, HEIGHT * 3))
-    WIN.blit(temp_win, (0,0))
+    WIN.blit(temp_win, (0, 0))
     pygame.display.update()
 
 
@@ -26,7 +24,63 @@ def update():
     pass
 
 
-def main():
+def intro():
+    titlescreen = pygame.image.load('sprites/GameJam Titel Engels.png')
+    titlescreen = pygame.transform.scale(titlescreen, (WIDTH * 3, HEIGHT * 3))
+    clock = pygame.time.Clock()
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = pygame.mouse.get_pos()
+                if 520 <= y <= 670:
+                    if 110 <= x <= 515:
+                        return
+                    if 1015 <= x <= 1430:
+                        pygame.quit()
+                        quit()
+
+        WIN.blit(titlescreen, (0, 0))
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+def pause():
+    menuscreen = pygame.image.load('sprites/GameJam menu.png')
+    menuscreen = pygame.transform.scale(menuscreen, (WIDTH * 3, HEIGHT * 3))
+    clock = pygame.time.Clock()
+    pause = True
+
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = pygame.mouse.get_pos()
+                if 444 <= x <= 1092:
+                    if 67 <= y <= 216:
+                        print("hi")
+                        return
+                if 562 <= x <= 973:
+                    if 260 <= y <= 407:
+                        main()
+                if 468 <= x <= 1068:
+                    if 450 <= y <= 597:
+                        game()
+                if 602 <= x <= 934:
+                    if 644 <= y <= 793:
+                        pygame.quit()
+                        quit()
+        WIN.blit(menuscreen, (0, 0))
+        pygame.display.update()
+        clock.tick(FPS)
+
+
+def game():
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -43,8 +97,16 @@ def main():
                     world.move(3)
                 if event.key == pygame.K_LEFT:
                     world.move(4)
+                if event.key == pygame.K_ESCAPE:
+                    pause()
+
         update()
         draw()
+
+
+def main():
+    intro()
+    game()
     pygame.quit()
 
 
